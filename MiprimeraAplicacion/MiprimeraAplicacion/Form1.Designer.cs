5ec8378db2c732439493e4973b6d60999a630a41
Jennifer Pacas <jenniferpacas2025@gmail.com>
2026-09-02 10:45:35 -0600

    casi terminado

--- MiprimeraAplicacion/MiprimeraAplicacion/Form1.Designer.cs
+++ MiprimeraAplicacion/MiprimeraAplicacion/Form1.Designer.cs
@@ -28,21 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDe = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResouesta = new System.Windows.Forms.Label();
+            this.cboDe = new System.Windows.Forms.ComboBox();
+            this.cboA = new System.Windows.Forms.ComboBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblCantidad = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Conversor de Area ";
+            // 
+            // lblDe
+            // 
+            this.lblDe.AutoSize = true;
+            this.lblDe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDe.Location = new System.Drawing.Point(21, 119);
+            this.lblDe.Name = "lblDe";
+            this.lblDe.Size = new System.Drawing.Size(46, 25);
+            this.lblDe.TabIndex = 1;
+            this.lblDe.Text = "De:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(297, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "A:";
+            // 
+            // lblResouesta
+            // 
+            this.lblResouesta.AutoSize = true;
+            this.lblResouesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResouesta.Location = new System.Drawing.Point(21, 257);
+            this.lblResouesta.Name = "lblResouesta";
+            this.lblResouesta.Size = new System.Drawing.Size(121, 25);
+            this.lblResouesta.TabIndex = 3;
+            this.lblResouesta.Text = "Respuesta:";
+            // 
+            // cboDe
+            // 
+            this.cboDe.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDe.FormattingEnabled = true;
+            this.cboDe.Items.AddRange(new object[] {
+            "Pie Cuadrado",
+            "Vara Cuadrada",
+            "Yarda Cuadrada",
+            "Metro Cuadrado",
+            "Tareas",
+            "Manzana",
+            "Hectárea"});
+            this.cboDe.Location = new System.Drawing.Point(73, 119);
+            this.cboDe.Name = "cboDe";
+            this.cboDe.Size = new System.Drawing.Size(183, 24);
+            this.cboDe.TabIndex = 4;
+            // 
+            // cboA
+            // 
+            this.cboA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboA.FormattingEnabled = true;
+            this.cboA.Items.AddRange(new object[] {
+            "Pie Cuadrado",
+            "Vara Cuadrada",
+            "Yarda Cuadrada",
+            "Metro Cuadrado",
+            "Tareas",
+            "Manzana",
+            "Hectárea"});
+            this.cboA.Location = new System.Drawing.Point(337, 119);
+            this.cboA.Name = "cboA";
+            this.cboA.Size = new System.Drawing.Size(183, 24);
+            this.cboA.TabIndex = 5;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(143, 190);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(113, 22);
+            this.txtTotal.TabIndex = 6;
+            this.txtTotal.TextChanged += new System.EventHandler(this.txtTotal_TextChanged);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(328, 219);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(145, 63);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(21, 186);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(106, 25);
+            this.lblCantidad.TabIndex = 8;
+            this.lblCantidad.Text = "Cantidad:";
+            this.lblCantidad.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 502);
+            this.ClientSize = new System.Drawing.Size(553, 432);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.cboA);
+            this.Controls.Add(this.cboDe);
+            this.Controls.Add(this.lblResouesta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblDe);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Combersores de longitud ";
+            this.Text = "Conversor de area";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDe;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResouesta;
+        private System.Windows.Forms.ComboBox cboDe;
+        private System.Windows.Forms.ComboBox cboA;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblCantidad;
     }
 }
 
--- MiprimeraAplicacion/MiprimeraAplicacion/Form1.cs
+++ MiprimeraAplicacion/MiprimeraAplicacion/Form1.cs
@@ -16,15 +16,43 @@ namespace MiprimeraAplicacion
         {
             InitializeComponent();
         }
-      
-        private void Form1_Load(object sender, EventArgs e)
+
+
+        /*
+    Pie Cuadrado
+    Vara Cuadrada
+    Yarda Cuadrada
+    Metro Cuadrado
+    Tareas
+    Manzana
+    Hectárea
+         */
+
+    Double[] valores={0.092903,0.698896,0.836127,1,437.5,6988.96,10000};
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int de = cboDe .SelectedIndex, a = cboA.SelectedIndex;
+
+            double cantidad = Double.Parse(txtTotal .Text);
+
+            double respuesta = valores[a] / valores[de] * cantidad;
+
+            lblResouesta.Text = "Respuesta: " + Math.Round(respuesta, 4).ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void btnCalcular_Click_2(object sender, EventArgs e)
+        private void txtTotal_TextChanged(object sender, EventArgs e)
         {
-           
+
         }
     }
 }
